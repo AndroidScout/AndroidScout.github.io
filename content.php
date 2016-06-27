07d90cb64362c6693c1b4d99fac7c624bf737ae9
Eran Machlev <eran.machlev@gmail.com>
2016-06-27 22:33:21 +0300

    Added a demo for the post content webpage

---
+++ content.php
@@ -0,0 +1,157 @@
+
+<?php
+
+//define global variables here
+
+?>
+
+
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AndroidScout</title>
+
+  <!-- Material Design Icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.css" rel="stylesheet">
+
+  <!-- Template style -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- Article Content style -->
+  <link href="css/ar.css" rel="stylesheet">
+
+</head>
+
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar z-depth-2 info-color">
+    <div class="container ">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand waves-effect waves-light" href="#">AndroidScout</a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li><a href="#" class="waves-effect waves-light">Home <span class="sr-only">(current)</span></a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" role="button" aria-expanded="false">ROM Reviews<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">A-D</a>
+              </li>
+              <li><a href="#">E-H</a>
+              </li>
+              <li><a href="#">I-P</a>
+              </li>
+              <li><a href="#">Q-V</a>
+              </li>
+              <li><a href="#">W-Z</a>
+              </li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" role="button" aria-expanded="false">Tech<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Phones</a>
+              </li>
+              <li><a href="#">Hardware</a>
+              </li>
+              <li><a href="#">Software</a>
+              </li>
+              <li><a href="#">Random Nonesense</a>
+              </li>
+              <li><a href="#">Rezs Dildos</a>
+              </li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" role="button" aria-expanded="false">Guest Work<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Concepts</a>
+              </li>
+              <li><a href="#">Reviews</a>
+              </li>
+              <li><a href="#">Photos</a>
+              </li>
+              <li><a href="#">Submit</a>
+              </li>
+            </ul>
+          </li>
+          <li><a href="#products" class="waves-effect waves-light">About</a></li>
+          <li><a href="#products" class="waves-effect waves-light">Support Us</a></li>
+        </ul>
+        <form class="navbar-form navbar-right waves-effect waves-light" role="search">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+
+  <!-- Page Heading -->
+
+
+  <div class="container">
+
+    <!-- Page Heading -->
+    <div class="row">
+      <div class="col-lg-12">
+      <h1 class="page-header" id="products" style="margin-bottom: 10px"><!--VARIABLE--> Cyanogen OS 12 Review</h1>
+      <h5 style="margin-top:0px">By <a href="#"><!--VARIABLE--> Eran Machlev</a></h5>
+      <h6 class="page-header" style="margin-top:0px; margin-bottom: 10px">Posted on <span><!--VARIABLE--> 23.06.2016</span></h6>
+
+      </div>
+    </div>
+
+    <!-- END Page HEading-->
+
+
+<!-- Article Content -->
+
+<img src="http://cdn.androidbeat.com/wp-content/uploads/2014/08/Cyanogen_logo.jpg" class="center-block z-depth-2 ar-image" style="max-width: 60%"/>
+
+</br></br>
+
+<!-- Article Text -->
+
+<div>
+
+<p align="left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac vulputate enim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisi lorem, porttitor ut molestie at, bibendum ac nisi. Vestibulum pellentesque, ex non rhoncus commodo, quam turpis rhoncus massa, eget finibus diam diam id urna. Fusce cursus iaculis aliquet. Nunc nulla tortor, malesuada ut egestas id, fringilla eget ex. Proin imperdiet aliquam quam, ac condimentum eros. Sed bibendum turpis neque, a dapibus nisl malesuada non. Aliquam porttitor tincidunt sem, sed bibendum mi cursus vitae. Aenean vestibulum ac dui eu maximus. Vestibulum sodales commodo semper.</p>
+<p align="left">Maecenas sollicitudin, mi sollicitudin mattis mattis, metus nisl tincidunt massa, consectetur laoreet eros tortor nec diam. In nulla nulla, malesuada nec efficitur sit amet, tincidunt et lectus. Nulla tincidunt felis sit amet eros tincidunt, quis vulputate libero eleifend. Quisque venenatis accumsan lorem in consequat. Fusce viverra ligula ac laoreet accumsan. Suspendisse sit amet porta ex. Suspendisse tortor odio, faucibus a facilisis eget, blandit vitae arcu. Suspendisse vel ligula turpis. Cras pharetra quis mi eu ullamcorper. Aliquam dictum sem nunc, quis feugiat est pharetra id. Nunc dictum nibh ipsum, quis tempor augue volutpat vitae. Morbi maximus, augue vel porttitor efficitur, velit odio imperdiet tellus, ac semper nulla tellus et metus. Suspendisse suscipit facilisis elit vitae maximus.</p>
+<img src="http://st1.bgr.in/wp-content/uploads/2015/04/yu-yureka-cyanogen-os-12-roll-out.jpg" class="center-block z-depth-2 ar-image" style="max-width: 60%"/>
+<p align="left">Duis iaculis condimentum tincidunt. Aliquam erat volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum laoreet tristique molestie. In vehicula, tellus a tristique imperdiet, sapien quam euismod mauris, vel suscipit metus tortor placerat lectus. Nullam massa lorem, venenatis eu convallis ac, egestas id magna. Donec ut venenatis quam. Fusce congue ligula nulla, et fringilla elit vehicula malesuada. Morbi dui libero, finibus sit amet risus in, laoreet dapibus turpis. Proin ut justo eros. In tincidunt imperdiet dapibus. Donec sed feugiat lectus. Nulla non congue justo.</p>
+<p align="left">Morbi quis pellentesque sem. Quisque massa orci, pulvinar nec ornare sed, venenatis vel tellus. Donec molestie imperdiet felis non ullamcorper. Cras porttitor non est non feugiat. Cras eu ex elit. Aenean sed leo sed turpis luctus tincidunt non at enim. Pellentesque dictum risus sed ex tempus, at pretium ante hendrerit. Nam ullamcorper sapien dui, laoreet laoreet turpis placerat a. Sed vulputate mattis ex. In feugiat dolor diam, eget rhoncus massa vestibulum ac. Fusce et metus sit amet turpis elementum hendrerit sit amet vel neque.</p>
+<p align="left">Mauris cursus, orci at mattis egestas, neque ex tristique est, eu luctus tortor urna eu justo. Nullam et justo nunc. Nullam non velit ac diam efficitur eleifend. Praesent mauris mauris, dapibus ac porta et, tempus eu nibh. Curabitur iaculis dui arcu. Nulla lorem velit, aliquam ac enim at, tempus sagittis leo. Curabitur a bibendum augue, sit amet fringilla nunc. Mauris sed porta quam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+
+</div>
+</br>
+</br>
+
+<!--END Article Text-->
+
+<!--END Article Content-->
+
+</body>
+</html>
